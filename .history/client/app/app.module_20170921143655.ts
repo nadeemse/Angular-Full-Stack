b9922b7ec123e0864a535668816b745ed05003e6
nadeemswich <nadeem@myswich.com>
2017-09-22 03:27:02 +0400

    adding first commit

---
+++ .history/client/app/app.module_20170921143655.ts
@@ -0,0 +1,49 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
+import { LayoutsModule } from './layouts/layouts.module';
+import { CatService } from './services/cat.service';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuardLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
+import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AccountComponent } from './account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatsComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    AdminComponent,
+    NotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    RoutingModule,
+    SharedModule,
+    LayoutsModule
+  ],
+  providers: [
+    AuthService,
+    AuthGuardLogin,
+    AuthGuardAdmin,
+    CatService,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ .history/client/app/app.module_20170921150852.ts
@@ -0,0 +1,50 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CatService } from './services/cat.service';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuardLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
+import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AccountComponent } from './account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatsComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    AdminComponent,
+    NotFoundComponent,
+    HeaderComponent
+    HomeComponent
+  ],
+  imports: [
+    RoutingModule,
+    SharedModule
+  ],
+  providers: [
+    AuthService,
+    AuthGuardLogin,
+    AuthGuardAdmin,
+    CatService,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
